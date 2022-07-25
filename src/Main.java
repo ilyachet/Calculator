ef656639bc92cf18ff0a8f0aeaa471935ca3d5b7
ilyachet <ilya.chet18@yandex.ru>
2022-07-25 16:14:39 +0400

    Just calculating

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(calc(s));
+    }
+
+        public static String calc(@NotNull String input) {
+            String[] all = input.split(" ");
+            int a = Integer.parseInt(all[0]);
+            int b = Integer.parseInt(all[2]);
+            char expr = all[1].charAt(0);
+            int result = calculation(a, b, expr);
+            return String.valueOf(result);
+        }
+
+        public static int calculation(int a, int b, char operation){
+            int result;
+            switch (operation){
+                case '+':
+                    result = a+b;
+                    break;
+                case '-':
+                    result = a-b;
+                    break;
+                case '*':
+                    result = a*b;
+                    break;
+                case '/':
+                    result = a/b;
+                    break;
+                default:
+                    result = 0;
+            }
+            return result;
+        }
+
+    }
